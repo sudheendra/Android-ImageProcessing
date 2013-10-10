bd97ef61a41a47fb43b4b12121444949fb5649b4
sudheendra <sudhindra.malli@gmail.com>
2013-10-10 10:52:05 +0530

    Adding more image conversion options.
    UI update, code for convolution matrix, other image effects.

--- ImageManip/src/main/java/com/image/imagemanip/ImageManipulator.java
+++ ImageManip/src/main/java/com/image/imagemanip/ImageManipulator.java
@@ -6,13 +6,27 @@ import android.graphics.Canvas;
 import android.graphics.Color;
 import android.graphics.ColorMatrix;
 import android.graphics.ColorMatrixColorFilter;
+import android.graphics.LinearGradient;
+import android.graphics.Matrix;
 import android.graphics.Paint;
+import android.graphics.Point;
 import android.graphics.PorterDuff;
+import android.graphics.PorterDuffXfermode;
+import android.graphics.Shader;
+
 
 /**
  * Created by sudheendra.sn on 9/20/13.
  */
 public class ImageManipulator {
+
+    public static final int FLIP_VERTICAL = 1;
+    public static final int FLIP_HORIZONTAL = 2;
+
+    public static final double PI = 3.14159d;
+    public static final double FULL_CIRCLE_DEGREE = 360d;
+    public static final double HALF_CIRCLE_DEGREE = 180d;
+    public static final double RANGE = 256d;
 
     private Bitmap modifiedImage;
 
@@ -237,5 +251,223 @@ public class ImageManipulator {
 
         // return final image
         return bmOut;
+    }
+
+    public static Bitmap applyGaussianBlur(Bitmap src) {
+        double[][] GaussianBlurConfig = new double[][] {
+                { 1, 2, 1 },
+                { 2, 4, 2 },
+                { 1, 2, 1 }
+        };
+        ConvolutionMatrix convMatrix = new ConvolutionMatrix(3);
+        convMatrix.applyConfig(GaussianBlurConfig);
+        convMatrix.Factor = 16;
+        convMatrix.Offset = 0;
+        return ConvolutionMatrix.computeConvolution3x3(src, convMatrix);
+    }
+
+    public static Bitmap sharpen(Bitmap src, double weight) {
+        double[][] SharpConfig = new double[][] {
+                { 0 , -2    , 0  },
+                { -2, weight, -2 },
+                { 0 , -2    , 0  }
+        };
+        ConvolutionMatrix convMatrix = new ConvolutionMatrix(3);
+        convMatrix.applyConfig(SharpConfig);
+        convMatrix.Factor = weight - 8;
+        return ConvolutionMatrix.computeConvolution3x3(src, convMatrix);
+    }
+
+    public static Bitmap smooth(Bitmap src, double value) {
+        ConvolutionMatrix convMatrix = new ConvolutionMatrix(3);
+        convMatrix.setAll(1);
+        convMatrix.Matrix[1][1] = value;
+        convMatrix.Factor = value + 8;
+        convMatrix.Offset = 1;
+        return ConvolutionMatrix.computeConvolution3x3(src, convMatrix);
+    }
+
+    public static Bitmap emboss(Bitmap src) {
+        double[][] EmbossConfig = new double[][] {
+                { -1 ,  0, -1 },
+                {  0 ,  4,  0 },
+                { -1 ,  0, -1 }
+        };
+        ConvolutionMatrix convMatrix = new ConvolutionMatrix(3);
+        convMatrix.applyConfig(EmbossConfig);
+        convMatrix.Factor = 1;
+        convMatrix.Offset = 127;
+        return ConvolutionMatrix.computeConvolution3x3(src, convMatrix);
+    }
+
+    public static Bitmap engrave(Bitmap src) {
+        ConvolutionMatrix convMatrix = new ConvolutionMatrix(3);
+        convMatrix.setAll(0);
+        convMatrix.Matrix[0][0] = -2;
+        convMatrix.Matrix[1][1] = 2;
+        convMatrix.Factor = 1;
+        convMatrix.Offset = 95;
+        return ConvolutionMatrix.computeConvolution3x3(src, convMatrix);
+    }
+
+    public static Bitmap boost(Bitmap src, int type, float percent) {
+        int width = src.getWidth();
+        int height = src.getHeight();
+        Bitmap bmOut = Bitmap.createBitmap(width, height, src.getConfig());
+
+        int A, R, G, B;
+        int pixel;
+
+        for(int x = 0; x < width; ++x) {
+            for(int y = 0; y < height; ++y) {
+                pixel = src.getPixel(x, y);
+                A = Color.alpha(pixel);
+                R = Color.red(pixel);
+                G = Color.green(pixel);
+                B = Color.blue(pixel);
+                if(type == 1) {
+                    R = (int)(R * (1 + percent));
+                    if(R > 255) R = 255;
+                }
+                else if(type == 2) {
+                    G = (int)(G * (1 + percent));
+                    if(G > 255) G = 255;
+                }
+                else if(type == 3) {
+                    B = (int)(B * (1 + percent));
+                    if(B > 255) B = 255;
+                }
+                bmOut.setPixel(x, y, Color.argb(A, R, G, B));
+            }
+        }
+        return bmOut;
+    }
+
+    public static Bitmap watermark(Bitmap src, String watermark, Point location, Color color, int alpha, int size, boolean underline) {
+        int w = src.getWidth();
+        int h = src.getHeight();
+        Bitmap result = Bitmap.createBitmap(w, h, src.getConfig());
+
+        Canvas canvas = new Canvas(result);
+        canvas.drawBitmap(src, 0, 0, null);
+
+        Paint paint = new Paint();
+        paint.setColor(color.hashCode());
+        paint.setAlpha(alpha);
+        paint.setTextSize(size);
+        paint.setAntiAlias(true);
+        paint.setUnderlineText(underline);
+        canvas.drawText(watermark, location.x, location.y, paint);
+
+        return result;
+    }
+
+    public static Bitmap flip(Bitmap src, int type) {
+        // create new matrix for transformation
+        Matrix matrix = new Matrix();
+        // if vertical
+        if(type == FLIP_VERTICAL) {
+            // y = y * -1
+            matrix.preScale(1.0f, -1.0f);
+        }
+        // if horizonal
+        else if(type == FLIP_HORIZONTAL) {
+            // x = x * -1
+            matrix.preScale(-1.0f, 1.0f);
+            // unknown type
+        } else {
+            return null;
+        }
+
+        // return transformed image
+        return Bitmap.createBitmap(src, 0, 0, src.getWidth(), src.getHeight(), matrix, true);
+    }
+
+    public static Bitmap tintImage(Bitmap src, int degree) {
+
+        int width = src.getWidth();
+        int height = src.getHeight();
+
+        int[] pix = new int[width * height];
+        src.getPixels(pix, 0, width, 0, 0, width, height);
+
+        int RY, GY, BY, RYY, GYY, BYY, R, G, B, Y;
+        double angle = (PI * (double)degree) / HALF_CIRCLE_DEGREE;
+
+        int S = (int)(RANGE * Math.sin(angle));
+        int C = (int)(RANGE * Math.cos(angle));
+
+        for (int y = 0; y < height; y++)
+            for (int x = 0; x < width; x++) {
+                int index = y * width + x;
+                int r = ( pix[index] >> 16 ) & 0xff;
+                int g = ( pix[index] >> 8 ) & 0xff;
+                int b = pix[index] & 0xff;
+                RY = ( 70 * r - 59 * g - 11 * b ) / 100;
+                GY = (-30 * r + 41 * g - 11 * b ) / 100;
+                BY = (-30 * r - 59 * g + 89 * b ) / 100;
+                Y  = ( 30 * r + 59 * g + 11 * b ) / 100;
+                RYY = ( S * BY + C * RY ) / 256;
+                BYY = ( C * BY - S * RY ) / 256;
+                GYY = (-51 * RYY - 19 * BYY ) / 100;
+                R = Y + RYY;
+                R = ( R < 0 ) ? 0 : (( R > 255 ) ? 255 : R );
+                G = Y + GYY;
+                G = ( G < 0 ) ? 0 : (( G > 255 ) ? 255 : G );
+                B = Y + BYY;
+                B = ( B < 0 ) ? 0 : (( B > 255 ) ? 255 : B );
+                pix[index] = 0xff000000 | (R << 16) | (G << 8 ) | B;
+            }
+
+        Bitmap outBitmap = Bitmap.createBitmap(width, height, src.getConfig());
+        outBitmap.setPixels(pix, 0, width, 0, 0, width, height);
+
+        pix = null;
+
+        return outBitmap;
+    }
+
+    public static Bitmap applyReflection(Bitmap originalImage) {
+        // gap space between original and reflected
+        final int reflectionGap = 4;
+        // get image size
+        int width = originalImage.getWidth();
+        int height = originalImage.getHeight();
+
+        // this will not scale but will flip on the Y axis
+        Matrix matrix = new Matrix();
+        matrix.preScale(1, -1);
+
+        // create a Bitmap with the flip matrix applied to it.
+        // we only want the bottom half of the image
+        Bitmap reflectionImage = Bitmap.createBitmap(originalImage, 0, height/2, width, height/2, matrix, false);
+
+        // create a new bitmap with same width but taller to fit reflection
+        Bitmap bitmapWithReflection = Bitmap.createBitmap(width, (height + height/2), Bitmap.Config.ARGB_8888);
+
+        // create a new Canvas with the bitmap that's big enough for
+        // the image plus gap plus reflection
+        Canvas canvas = new Canvas(bitmapWithReflection);
+        // draw in the original image
+        canvas.drawBitmap(originalImage, 0, 0, null);
+        // draw in the gap
+        Paint defaultPaint = new Paint();
+        canvas.drawRect(0, height, width, height + reflectionGap, defaultPaint);
+        // draw in the reflection
+        canvas.drawBitmap(reflectionImage,0, height + reflectionGap, null);
+
+        // create a shader that is a linear gradient that covers the reflection
+        Paint paint = new Paint();
+        LinearGradient shader = new LinearGradient(0, originalImage.getHeight(), 0,
+                bitmapWithReflection.getHeight() + reflectionGap, 0x70ffffff, 0x00ffffff,
+                Shader.TileMode.CLAMP);
+        // set the paint to use this shader (linear gradient)
+        paint.setShader(shader);
+        // set the Transfer mode to be porter duff and destination in
+        paint.setXfermode(new PorterDuffXfermode(PorterDuff.Mode.DST_IN));
+        // draw a rectangle using the paint with our linear gradient
+        canvas.drawRect(0, height, width, bitmapWithReflection.getHeight() + reflectionGap, paint);
+
+        return bitmapWithReflection;
     }
 }
